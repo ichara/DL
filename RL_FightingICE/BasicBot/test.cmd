2fdd7f837b32e956e936cab5b5dd74349fca31e4
ichara <ichara.com@gmail.com>
2019-04-14 22:17:21 +0900

    Add BasicBot with 'distance' parameter (again)

---
+++ RL_FightingICE/BasicBot/test.cmd
@@ -0,0 +1,1 @@
+python3 BasicBot.py test --max-epoch 5 --training-step 10000 --evaluation-sec 60 --opponent=Machete --n-cpu 2 --path output/test02 --render single --monitor matplotlib
---
+++ RL_FightingICE/BasicBot/train.cmd
@@ -0,0 +1,1 @@
+python3 BasicBot.py train --max-epoch 5 --training-step 120000 --evaluation-sec 120 --opponent=Machete --n-cpu 2 --path output/mytest60 --render single --monitor matplotlib
